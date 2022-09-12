0ab1d68a82c1ef228acce937184b83b23d7e68c1
Felipe Ribeiro <piporibeiro@gmail.com>
2022-09-12 18:49:35 -0300

    chore: configure node

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', () => {
-    console.log('Running')
+app.get('/ads', (request, response) => {
+    return response.json({})
 })
 
 app.listen(3333)
